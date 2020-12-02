a2deb7b23ac760004537b85182b1d3838428093a
Akshat Atray <64903530+akshatatray@users.noreply.github.com>
2020-12-02 20:25:20 +0530

    Protected Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Home from './Components/Home/Home.jsx';
 import Cart from './Components/Cart/Cart.jsx';
 import Menu from './Components/Menu/Menu.jsx';
 import Loader from './Loader/Loader';
 
+import Private from './Routes/Private';
+import Public from './Routes/Public';
+
 function App() {
   return (
     <Router>
       <Loader />
-      <Route path="/" exact component={ Home } />
-      <Route path="/cart" exact component={ Cart } />
-      <Route path="/shop/:id" exact component={ Menu } />
+      <Private path="/" exact component={ Home } />
+      <Private path="/cart" exact component={ Cart } />
+      <Private path="/shop/:id" exact component={ Menu } />
     </Router>
   );
 }
